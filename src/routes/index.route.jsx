a6f1cb46cab54a3176dab2cba38e3c22a95ef7e8
Conor Thompson <conorthompson213@gmail.com>
2023-11-08 20:37:34 +0000

    minor bug fix

--- src/routes/index.route.jsx
+++ src/routes/index.route.jsx
@@ -33,7 +33,7 @@ const HomeRoute = () => {
           <Card.Title>Search for anything on the internet.</Card.Title>
           <Form
             name="s"
-            onSubmit={search}
+            onSubmit={() => search}
             onChange={(e) => setSearchTerm(e.target.value)}
           >
             <Form.Group className="mb-3">
